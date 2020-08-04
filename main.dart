67c9233a660098467569efd37309a8d17ac7cf1a
mansidas19 <62935129+mansidas19@users.noreply.github.com>
2020-08-04 08:16:43 +0530

    video_app

--- main.dart
+++ main.dart
@@ -1,142 +1,128 @@
-import 'package:audioplayers/audioplayers.dart';
-import 'package:file_picker/file_picker.dart';
+import 'package:chewie/chewie.dart';
+import 'package:chewie/src/chewie_player.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:video_player/video_player.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(
+    ChewieDemo(),
+  );
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class ChewieDemo extends StatefulWidget {
+  ChewieDemo({this.title = 'RAY OF HOPE'});
+
+  final String title;
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
-      debugShowCheckedModeBanner: false,
-    );
+  State<StatefulWidget> createState() {
+    return _ChewieDemoState();
   }
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  AudioPlayer _audioPlayer = AudioPlayer();
-  bool isPlaying = false;
-  String currentTime = "00:00";
-  String completeTime= "00:00";
+class _ChewieDemoState extends State<ChewieDemo> {
+  TargetPlatform _platform;
+  VideoPlayerController _videoPlayerController1;
+  VideoPlayerController _videoPlayerController2;
+  ChewieController _chewieController;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    _videoPlayerController1 = VideoPlayerController.asset('assets/IMG_9250.MP4');
+    _videoPlayerController2 = VideoPlayerController.network('https://drive.google.com/file/d/19hdWcpOfTTpC8Ju298GxEuDhz5uM9HzA/view?usp=sharing');
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController2,
+      aspectRatio: 16/9,
+      autoPlay: true,
+      looping: true,
+      // Try playing around with some of these other options:
 
-    _audioPlayer.onAudioPositionChanged.listen((Duration duration){
-      setState(() {
-        currentTime = duration.toString().split(".")[0];
-      });
-    });
-
-    _audioPlayer.onDurationChanged.listen((Duration duration){
-      setState(() {
-        completeTime = duration.toString().split(".")[0];
-      });
-    });
-
+      // showControls: false,
+      // materialProgressColors: ChewieProgressColors(
+      //   playedColor: Colors.red,
+      //   handleColor: Colors.blue,
+      //   backgroundColor: Colors.grey,
+      //   bufferedColor: Colors.lightGreen,
+      // ),
+      // placeholder: Container(
+      //   color: Colors.grey,
+      // ),
+      // autoInitialize: true,
+    );
   }
 
   @override
+  void dispose() {
+    _videoPlayerController1.dispose();
+    _videoPlayerController2.dispose();
+    _chewieController.dispose();
+    super.dispose();
+  }
+
+  msg(){
+    Fluttertoast.showToast(
+        msg: "MAKING INDIA \n FUTURE READY",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 5,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 25.0
+    );
+  }
+  
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.purpleAccent,
-        title: Text('Enjoy the Music',style: TextStyle(fontStyle: FontStyle.italic, fontSize: 25,),),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: widget.title,
+      theme: ThemeData.light().copyWith(
+        platform: _platform ?? Theme.of(context).platform,
       ),
-      backgroundColor: Colors.black,
-      body: Stack(
-        children: <Widget>[
-
-          Image.asset("assets/music.jpg", fit: BoxFit.contain,),
-
-          Container(
-            width: MediaQuery.of(context).size.width*0.8,
-            height: 80,
-
-            margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.7, left: MediaQuery.of(context).size.width*0.1),
-
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(50)
-            ),
-
-
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
-                  onPressed: (){
-                    if(isPlaying){
-                      _audioPlayer.pause();
-
-                      setState(() {
-                        isPlaying = false;
-                      });
-                    }else{
-                      _audioPlayer.resume();
-
-
-                      setState(() {
-                        isPlaying = true;
-                      });
-                    }
-
-                  },
-                )  ,
-
-                SizedBox(width: 16,),
-
-                IconButton(
-                  icon: Icon(Icons.stop),
-                  onPressed: (){
-                    _audioPlayer.stop();
-
-                    setState(() {
-                      isPlaying = false;
-                    });
-
-                  },
-                ),
-
-                Text(currentTime, style: TextStyle(fontWeight: FontWeight.w700),),
-
-                Text(" | "),
-
-                Text(completeTime, style: TextStyle(fontWeight: FontWeight.w300),),
-                ],
-            ),
+      home: Scaffold(
+        appBar: AppBar(
+          actions: <Widget>[IconButton(icon: Icon(Icons.message),onPressed: msg,)],
+          backgroundColor: Colors.amberAccent,
+          title: Text('RAY OF HOPE',style: TextStyle(fontStyle: FontStyle.italic, fontFamily: 'Raleway',color: Colors.redAccent, fontWeight: FontWeight.bold,fontSize: 30),),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+    color: Colors.orange,
+    borderRadius: BorderRadius.only(
+      topLeft: Radius.circular(200),
+        topRight: Radius.circular(200),
+        bottomLeft: Radius.circular(200),
+        bottomRight: Radius.circular(200)
+    ),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.red.withOpacity(1),
+        spreadRadius: 20,
+        blurRadius: 10,
+        offset: Offset(0, 3), // changes position of shadow
+      ),
+    ],
           ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.audiotrack),
-        onPressed: () async{
-          String filePath = await FilePicker.getFilePath();
-
-          int status = await _audioPlayer.play(filePath, isLocal: true);
-
-          if(status == 1){
-            setState(() {
-              isPlaying = true;
-            });
-          }
-        },
+          width:double.infinity,
+          height: double.infinity,
+          margin: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: Center(
+                  child: Chewie(
+                    controller: _chewieController,
+                  ),
+                ),
+              ),
+              
+            ],
+          ),
+        ),
       ),
     );
   }
